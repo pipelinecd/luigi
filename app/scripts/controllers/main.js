2b16a1050f64e0a41881630da0523da7935785e6
Ronald van Eede <rveede@tricode.nl>
2013-12-29 13:40:30 +0100

    Added labels

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,6 +3,17 @@
 angular.module('luigiApp')
   .controller('MainCtrl', function ($scope, $http) {
     //$http.get('http://api.admin.pipelinecd.com/pipelines').success(function(data) {
-        $scope.pipelines = [{name: 'Pipeline 1', status: 'Running'}, {name: 'Pipeline 2', status: 'Failed'}];
+        $scope.pipelines = [{name: 'Pipeline 1', status: 'running'}, {name: 'Pipeline 2', status: 'failed'}, {name: 'Pipeline 3', status: 'success'}];
     //});
+
+    $scope.whatClassIsIt= function(someValue){
+        if(someValue=="running")
+            return "label label-primary"
+        else if(someValue=="failed")
+            return "label label-danger";
+        else if(someValue=="success")
+            return "label label-success";
+        else
+            return "label label-default";
+    }
   });
